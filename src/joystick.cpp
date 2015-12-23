95a359db38910460eb2d2b0efce8cbd63b0abe6f
Mike Purvis <mpurvis@clearpathrobotics.com>
2015-12-23 13:07:39 -0500

    Switch to SDL2, still no dice on OS X.

--- src/joystick.cpp
+++ src/joystick.cpp
@@ -1,16 +1,34 @@
-#include <SDL.h>
+#include "SDL_gamecontroller.h"
+#include "SDL.h"
 
 int main(int argc, char *argv[])
 {
-  if (SDL_Init(SDL_INIT_JOYSTICK))
+  if (SDL_Init(SDL_INIT_GAMECONTROLLER | SDL_INIT_JOYSTICK))
   {
+    printf("SDL_Init failed.\r\n");
     return 1;
   }
-  int num_joy, i;
-  num_joy=SDL_NumJoysticks();
+  int ret = SDL_GameControllerAddMappingsFromFile("SDL_GameControllerDB/gamecontrollerdb.txt");
+  printf("ret %d\r\n", ret);
+  SDL_JoystickEventState(SDL_DISABLE);
+
+  int num_joy=SDL_NumJoysticks();
   printf("%d joysticks found\n", num_joy);
-  for(i=0;i<num_joy;i++)
-      printf("%s\n", SDL_JoystickName(i));
+
+  SDL_Joystick* joystick = SDL_JoystickOpen(0);
+  if (joystick == NULL)
+  {
+    printf("Joystick not opened.\r\n");
+    return 1;
+  }
+
+  while(true)
+  {
+    SDL_JoystickUpdate();
+    int16_t x = SDL_JoystickGetAxis(joystick, 0);
+
+    printf("x: %d\r\n", x);
+  }
 
   SDL_Quit();
   return 0;
